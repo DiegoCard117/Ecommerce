69238c81262854420aca49632c97572814060dc2
DiegoCard117 <diegocardoso117@gmail.com>
2023-08-10 00:38:44 -0300

    menu feito

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,32 +1,31 @@
-import gpu from '../img/gpu.svg'
-import mouse from '../img/mouse.svg'
-import desktop from '../img/desktop.svg'
-import upgrade from '../img/upgrade.svg'
-import monitor from '../img/monitor.svg'
-import cadeira from '../img/cadeira.svg'
-import mesa from '../img/mesa.svg'
-import note from '../img/note.svg'
-import console from '../img/console.svg'
-import router from '../img/router.svg'
+/*
 
-import Image from 'next/image'
+function Aside() {
 
-const imagens = [
-  {id:'1', img: gpu},
-  {id:'2', img: mouse},
-  {id:'3', img: desktop},
-  {id:'4', img: upgrade},
-  {id:'5', img: monitor},
-  {id:'6', img: cadeira},
-  {id:'7', img: mesa},
-  {id:'8', img: note},
-  {id:'9', img: console},
-  {id:'10', img: router}
-]
+  const [MenuClass, setMenuClass] = useState('close')
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  
+!isMenuOpen ? setMenuClass("open") : setMenuClass("close")
 
-export default function Aside() {
+setIsMenuOpen(!isMenuOpen)
+
+
+const updateMenu = () => {
+  const aside = document.querySelector('aside');
+  if (aside) {
+    if (!isMenuOpen) {
+      aside.style.width = '100%';
+      setMenuClass('open');
+    } else {
+      setMenuClass('close');
+      aside.style.width = '90px';
+    }
+    setIsMenuOpen(!isMenuOpen);
+  }
+}
+
   return (
-      <aside>
+      <aside onClick={updateMenu}>
         <div className='menu-left'>
           {imagens.map( (item) => (
             <button key={item.id}>
@@ -37,7 +36,7 @@ export default function Aside() {
             </button>
           ))}
         </div>
-        <div className='open'>
+        <div className={MenuClass}>
           <ul>
             <li>Hardware</li>
             <li>Perifericos</li>
@@ -54,3 +53,4 @@ export default function Aside() {
       </aside>
   )
 }
+*/
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,48 @@
+"use client";
 import menu from '../img/menu.svg'
 import cart from '../img/cart.svg'
 import favorites from '../img/heart.svg'
 
+import gpu from '../img/gpu.svg'
+import mouse from '../img/mouse.svg'
+import desktop from '../img/desktop.svg'
+import upgrade from '../img/upgrade.svg'
+import monitor from '../img/monitor.svg'
+import cadeira from '../img/cadeira.svg'
+import mesa from '../img/mesa.svg'
+import note from '../img/note.svg'
+import console from '../img/console.svg'
+import router from '../img/router.svg'
+
 import Image from 'next/image'
+import { useState } from 'react';
 
-import '../css/header.scss'
-
-import Aside from './Aside';
-
+const imagens = [
+  {id:'1', img: gpu},
+  {id:'2', img: mouse},
+  {id:'3', img: desktop},
+  {id:'4', img: upgrade},
+  {id:'5', img: monitor},
+  {id:'6', img: cadeira},
+  {id:'7', img: mesa},
+  {id:'8', img: note},
+  {id:'9', img: console},
+  {id:'10', img: router}
+]
 
 export default function Header() {
+
+  const [MenuClass, setMenuClass] = useState('close')
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const updateMenu = () => {
+    const aside = document.querySelector('aside');
+    if (aside) {
+      !isMenuOpen ? setMenuClass("open") : setMenuClass("close")
+      setIsMenuOpen(!isMenuOpen)
+    }
+  }
+
   return (
     <>
       <header>
@@ -32,17 +65,42 @@ export default function Header() {
             </div>
           </div>
           <div className='menu-bottom'>
-            <button>
+            <button onClick={updateMenu}>
             <Image
                 src={menu}
-                alt='favoritos'
+                alt='menu'
               />
             </button>
             <input type="text" id="search" className='search'/>
           </div>
         </nav>
       </header>
-      <Aside/>
+      <aside>
+        <div className='menu-left' onClick={updateMenu}>
+          {imagens.map( (item) => (
+            <button key={item.id}>
+              <Image
+                src={item.img}
+                alt='menu'
+              />
+            </button>
+          ))}
+        </div>
+        <div className={MenuClass}>
+          <ul>
+            <li>Hardware</li>
+            <li>Perifericos</li>
+            <li>Computadores</li>
+            <li>Kit Upgrade</li>
+            <li>Monitores</li>
+            <li>Cadeiras e Mesas Gamer</li>
+            <li>Cadeiras e Mesas de Escritorio</li>
+            <li>Notebooks</li>
+            <li>Consoles</li>
+            <li>Redes e Wireless</li>
+          </ul>
+        </div>
+      </aside>
     </>
   )
 }
