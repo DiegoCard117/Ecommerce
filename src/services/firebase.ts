c070fe3c9b426c6f3fe97d0c29115066bf969093
DiegoCard117 <diegocardoso117@gmail.com>
2023-08-28 13:23:23 -0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/compat/auth'
 import 'firebase/compat/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAx-caMGKC9Jz5JDdqHzu77Tr3tLu4CMVg",
-    authDomain: "ecommerce-723f4.firebaseapp.com",
-    databaseURL: "https://ecommerce-723f4-default-rtdb.firebaseio.com",
-    projectId: "ecommerce-723f4",
-    storageBucket: "ecommerce-723f4.appspot.com",
-    messagingSenderId: "252477395131",
-    appId: "1:252477395131:web:87e56ff2c6f95e7a4ad99e"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig)
